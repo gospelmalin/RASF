8538f953e641406c028c0be5649e242e38fea5cc
Malin Albinsson <mea733@telia.com>
2019-01-30 20:01:48 +0100

    Added CategoryService class first version

---
+++ src/com/gospelmalin/rasf/CategoryService.java
@@ -0,0 +1,26 @@
+package com.gospelmalin.rasf;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+//Själva webservicen
+@Path("/CategoryService")
+public class CategoryService {
+
+	CategoryDao categoryDao = new CategoryDao();
+	   private static final String SUCCESS_RESULT="<result>success</result>";
+	   private static final String FAILURE_RESULT="<result>failure</result>";
+
+	   
+	   @GET
+	   @Path("/categories")
+	   @Produces(MediaType.APPLICATION_XML)
+	   public List<Category> getCategories(){
+	      return categoryDao.getAll();
+	   }
+
+}
