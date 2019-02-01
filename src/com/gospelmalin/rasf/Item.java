1c34836d523be1ee6a8c7fe3af2b5eda9655dc39
Malin Albinsson <mea733@telia.com>
2019-02-01 17:49:28 +0100

    Updated Item to reflect new database structure

--- src/com/gospelmalin/rasf/Item.java
+++ src/com/gospelmalin/rasf/Item.java
@@ -14,6 +14,14 @@ public class Item implements Serializable {
 	private String itemName;
 	private int unitsAlways;
 	private String available;
+	
+	private int numberOfUnits;
+	//private int contentKey; //Old version
+	
+	private String categoryName;
+	
+	private int storageplaceKey; // Currently not entirely implemented
+	private String storageplaceName; // Currently not entirely implemented
 
 	// Constructors
 	public Item(){}
@@ -38,7 +46,44 @@ public class Item implements Serializable {
 		this.available = available;
 	}
 	
-	// Getters and setters
+	public Item(int itemKey, int categoryKey, String itemName, int unitsAlways, String available, int numberOfUnits, int contentKey, String categoryName) {
+		super();
+		this.itemKey = itemKey;
+		this.categoryKey = categoryKey;
+		this.itemName = itemName;
+		this.unitsAlways = unitsAlways;
+		this.available = available;
+		this.numberOfUnits = numberOfUnits;
+		//this.contentKey = contentKey;
+		this.categoryName = categoryName;
+	}
+	
+
+	
+	public Item(int itemKey, int categoryKey, String itemName, int unitsAlways, String available, int numberOfUnits,
+			String categoryName, int storageplaceKey, String storageplaceName) {
+		super();
+		this.itemKey = itemKey;
+		this.categoryKey = categoryKey;
+		this.itemName = itemName;
+		this.unitsAlways = unitsAlways;
+		this.available = available;
+		this.numberOfUnits = numberOfUnits;
+		this.categoryName = categoryName;
+		this.storageplaceKey = storageplaceKey;
+		this.storageplaceName = storageplaceName;
+	}
+	
+	public Item(int itemKey, String itemName, int unitsAlways, String available, int numberOfUnits) {
+		super();
+		this.itemKey = itemKey;
+		this.itemName = itemName;
+		this.unitsAlways = unitsAlways;
+		this.available = available;
+		this.numberOfUnits = numberOfUnits;
+}
+
+		// Getters and setters
 		public int getItemKey() {
 			return itemKey;
 		}
@@ -85,6 +130,52 @@ public class Item implements Serializable {
 			this.available = available;
 		}
 		
+		public int getNumberOfUnits() {
+			return numberOfUnits;
+		}
+		
+		@XmlElement
+		public void setNumberOfUnits(int numberOfUnits) {
+			this.numberOfUnits = numberOfUnits;
+		}
+
+		/*
+		public int getContentKey() {
+			return contentKey;
+		}
+		
+		@XmlElement
+		public void setContentKey(int contentKey) {
+			this.contentKey = contentKey;
+		}
+*/
+		public String getCategoryName() {
+			return categoryName;
+		}
+		
+		@XmlElement
+		public void setCategoryName(String categoryName) {
+			this.categoryName = categoryName;
+		}
+
+		public int getStorageplaceKey() {
+			return storageplaceKey;
+		}
+		
+		@XmlElement
+		public void setStorageplaceKey(int storageplaceKey) {
+			this.storageplaceKey = storageplaceKey;
+		}
+
+		public String getStorageplaceName() {
+			return storageplaceName;
+		}
+		
+		@XmlElement
+		public void setStorageplaceName(String storageplaceName) {
+			this.storageplaceName = storageplaceName;
+		}
+
 		@Override
 		   public boolean equals(Object object){
 		      
