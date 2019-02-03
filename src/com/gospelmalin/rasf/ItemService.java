f4e83551c4cfbe4e59235dd465fcfb913924e73e
Malin Albinsson <mea733@telia.com>
2019-02-03 12:33:15 +0100

    ItemService comments updated.

--- src/com/gospelmalin/rasf/ItemService.java
+++ src/com/gospelmalin/rasf/ItemService.java
@@ -7,7 +7,6 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.OPTIONS;
-import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -19,7 +18,9 @@ import javax.ws.rs.core.MediaType;
 
 
 
-//Själva webservicen
+/**
+ * The Class ItemService is the actual webservice for item.
+ */
 @Path("/ItemService")
 public class ItemService {
 	
@@ -27,7 +28,10 @@ public class ItemService {
 	   private static final String SUCCESS_RESULT="<result>success</result>";
 	   private static final String FAILURE_RESULT="<result>failure</result>";
 
-	   
+	   /**
+	    * Show all items
+	    *
+	    */
 	   @GET
 	   @Path("/items")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -35,7 +39,10 @@ public class ItemService {
 	      return itemDao.getAll();
 	   }
 	   
-	   
+	   /**
+	    * Show information about selected item
+	    *
+	    */
 	   @GET
 	   @Path("/items/{itemKey}")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -43,6 +50,10 @@ public class ItemService {
 	      return itemDao.getItem(itemKey);
 	   }
 	   
+	   /**
+	    * Show all items for selected category
+	    *
+	    */
 	   @GET
 	   @Path("/categories/{categoryKey}/items")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -50,6 +61,10 @@ public class ItemService {
 	      return itemDao.getAllForCategory(categoryKey);
 	   }
 	   
+	   /**
+	    * Show all items for selected storageplace
+	    *
+	    */
 	   @GET
 	   @Path("/storageplaces/{storageplaceKey}/items")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -57,6 +72,11 @@ public class ItemService {
 	      return itemDao.getAllForStorageplace(storageplaceKey);
 	   }
 	   
+	   /**
+		 * Update an item - update units always at home,  update number of units when adding or removing items 
+		 * from a storageplace, change storageplace or category for an item
+		 *
+		 */
 	   @PUT
 	   @Path("/items/{itemKey}")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -71,7 +91,6 @@ public class ItemService {
 	   		   @QueryParam("storageplaceName") String storageplaceName,
 	      @Context HttpServletResponse servletResponse) throws IOException{
 	      Item item = new Item(itemKey, categoryKey, itemName, unitsAlways, available, numberOfUnits, categoryName, storageplaceKey, storageplaceName);
-	    //  Item item = new Item(itemKey, itemName, unitsAlways, available, numberOfUnits);
 	      int result = itemDao.updateItem(item);
 	      if(result == 1){
 	         return SUCCESS_RESULT;
@@ -79,6 +98,10 @@ public class ItemService {
 	      return FAILURE_RESULT;
 	   }
 	   
+	   /**
+		 * Add a new item
+		 *
+		 */
 	   @POST
 	   @Path("/items")
 	   @Produces(MediaType.APPLICATION_XML)
@@ -89,9 +112,9 @@ public class ItemService {
 			   @QueryParam("unitsAlways") int unitsAlways, 
 			   @QueryParam("available") String available, 
 			   @QueryParam("numberOfUnits") int numberOfUnits,   
-			//   @QueryParam("categoryName") String categoryName,
+			// @QueryParam("categoryName") String categoryName,
 			   @QueryParam("storageplaceKey") int storageplaceKey,
-	   		//   @QueryParam("storageplaceName") String storageplaceName,
+	   		// @QueryParam("storageplaceName") String storageplaceName,
 	      @Context HttpServletResponse servletResponse) throws IOException{
 		   Item item = new Item(categoryKey, itemName, unitsAlways, available, numberOfUnits, storageplaceKey);
 	      int result = itemDao.addItem(item);
@@ -101,36 +124,15 @@ public class ItemService {
 	      return FAILURE_RESULT;
 	   }
 	   
-	   /*
-	   @PUT
-	   @Path("/items")
-	   @Produces(MediaType.APPLICATION_XML)
-	   @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-	   public String updateItem(@FormParam("itemKey") int itemKey,
-		//	   @FormParam("categoryKey") int categoryKey, 
-			   @FormParam("itemName") String itemName, 
-			   @FormParam("unitsAlways") int unitsAlways, 
-			   @FormParam("available") String available, 
-			   @FormParam("numberOfUnits") int numberOfUnits,   
-			   //@FormParam("categoryName") String categoryName,
-			//   @FormParam("storageplaceKey") int storageplaceKey,
-	   		//   @FormParam("storageplaceName") String storageplaceName,
-	      @Context HttpServletResponse servletResponse) throws IOException{
-	  //    Item item = new Item(itemKey, categoryKey, itemName, unitsAlways, available, numberOfUnits, categoryName, storageplaceKey, storageplaceName);
-	      Item item = new Item(itemKey, itemName, unitsAlways, available, numberOfUnits);
-	      int result = itemDao.updateItem(item);
-	      if(result == 1){
-	         return SUCCESS_RESULT;
-	      }
-	      return FAILURE_RESULT;
-	   }
-	   */
-
+	 
+	   /**
+	    * Show options for the item service
+	    *
+	    */
 	   @OPTIONS
 	   @Path("/items")
 	   @Produces(MediaType.APPLICATION_XML)
 	   public String getSupportedOperations(){
-	      //return "<operations>GET, PUT, POST, DELETE</operations>";
 		   return "<operations>GET, PUT, POST</operations>"; //DELETE FROM ITEM NOT ALLOWED
 	   }
 	   
