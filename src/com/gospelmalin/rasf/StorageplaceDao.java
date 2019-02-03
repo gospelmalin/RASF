06e7c7b0a7e947c10153700c6a7c3fec556e1193
Malin Albinsson <mea733@telia.com>
2019-02-03 12:48:09 +0100

    StorageplaceDAO updated comments. Housekeeping

--- src/com/gospelmalin/rasf/StorageplaceDao.java
+++ src/com/gospelmalin/rasf/StorageplaceDao.java
@@ -7,20 +7,22 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * The Class StorageplaceDao handles the storageplace related queries for the database, isolating the database from the business layer.
+ */
 public class StorageplaceDao {
 	
+	/**
+    * Show all storageplaces
+    *
+    */
 	public List<Storageplace> getAll() {
-		List<Storageplace> storageplacesList = null; 
-	      
+		List<Storageplace> storageplacesList = null;   
 		storageplacesList = new ArrayList<Storageplace>();
-		
-		
 		Database db = new Database();
 		try {
 			String query = "SELECT * from storageplace;";
 			ResultSet rs = db.executeQuery(query);
-	  
-			
 			while(rs.next()) {
 			  int storageplaceKey = rs.getInt("storageplace_key");
 			  String storageplaceName = rs.getString("storageplace_name");
@@ -28,16 +30,16 @@ public class StorageplaceDao {
 			  storageplacesList.add(new Storageplace(storageplaceKey, storageplaceName));
 			}
 		} catch (SQLException e) {
-		// TODO Auto-generated catch block
-			e.printStackTrace();
+			System.err.println("An SQL exception occured when trying to get all storageplaces " + e.getMessage());
 		}
-		
 	      return storageplacesList; 
 	   } 
-
+	/**
+    * Show information about selected storageplace
+    *
+    */
 	public Storageplace getStorageplace(int storageplaceKey){
-		   List<Storageplace> storageplaces = getAll();
-
+		  List<Storageplace> storageplaces = getAll();
 	      for(Storageplace storageplace: storageplaces){
 	         if(storageplace.getStorageplaceKey() == storageplaceKey){
 	            return storageplace;
@@ -46,6 +48,10 @@ public class StorageplaceDao {
 	      return null;
 	   }
 
+	/**
+	 * Add a new storageplace
+	 *
+	 */
 	public int addStorageplace (Storageplace pStorageplace){
 	      List<Storageplace> storageplaceList = getAll();
 	      boolean storageplaceExists = false;
@@ -63,144 +69,94 @@ public class StorageplaceDao {
 	         try {
 				// Setup statement
 				 PreparedStatement stmt = conn.prepareStatement(query);
-
 				 // Set values
-				stmt.setString(1, pStorageplace.getStorageplaceName());
-								
+				stmt.setString(1, pStorageplace.getStorageplaceName());						
 				// Execute statement
-				stmt.executeUpdate();
-				
+				stmt.executeUpdate();				
 				// Closing statement and connection
 				stmt.close();
 				Database.mariaDbClose();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
+				System.err.println("An SQL exception occured when trying to add a record to storageplace " + e.getMessage());
 				return 0;
-			}
-		      
+			}		      
 	         return 1;
 	      }
 	      return 0;
 	   }
 	
+	/**
+	 * Update storageplace name
+	 *
+	 */
 	public int updateStorageplace(Storageplace pStorageplace){
 	      List<Storageplace> storageplaceList = getAll();
-
 	      for(Storageplace storageplace: storageplaceList){
 	         if(storageplace.getStorageplaceKey() == pStorageplace.getStorageplaceKey()){
-	            int index = storageplaceList.indexOf(storageplace);			
-	            storageplaceList.set(index, pStorageplace);
-
+	           int index = storageplaceList.indexOf(storageplace);			
+	           storageplaceList.set(index, pStorageplace);
 	           String query = "UPDATE storageplace SET storageplace_name = ? WHERE storageplace_key = ?;";
 			       
-	            Connection conn = Database.connectMariaDb();
+	           Connection conn = Database.connectMariaDb();
 		         try {
 					// Setup statement
-					 PreparedStatement stmt = conn.prepareStatement(query);
-	     
+					 PreparedStatement stmt = conn.prepareStatement(query);	
 					 // Set values
 					 stmt.setString(1, pStorageplace.getStorageplaceName());
-					 stmt.setInt(2, pStorageplace.getStorageplaceKey());
-										
+					 stmt.setInt(2, pStorageplace.getStorageplaceKey());										
 					// Execute statement
-					stmt.executeUpdate();
-					
+					stmt.executeUpdate();					
 					// Closing statement and connection
 					stmt.close();
 					Database.mariaDbClose();
 				} catch (SQLException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					System.err.println("An SQL exception occured when trying to update a record in storageplace " + e.getMessage());
 					return 0;
-				}
-	            
+				}	            
 	            return 1;
 	         }
 	      }		
 	      return 0;
 	   }
 	
-	// Note that deleting a storageplace will remove also any linked items!
-		public int deleteStorageplace(int storageplaceKey){
-			
+		/**
+	    * Delete a storageplace.
+	    * Delete only allowed for storageplaces with no linked items,
+	    * as deleting a storageplace otherwise would also delete those items.
+	    *
+	    */
+		public int deleteStorageplace(int storageplaceKey){			
 			 ItemDao itemDao = new ItemDao();
 			  List<Item> itemList = itemDao.getAll();
-		//Check if storageplace is used by any item(s)
+			 //Check if storageplace is used by any item(s)
 			  boolean storageplaceUsed = false;
 		      for(Item item: itemList){
 		        if(item.getStorageplaceKey() == storageplaceKey){
 		        	storageplaceUsed = true;
-			      	 System.out.println("Storageplace may only be deleted while not assigned to item(s). Please reassign item(s) and retry.");
+			      	System.out.println("Storageplace may only be deleted while not assigned to item(s). Please reassign item(s) and retry.");
 			          break;
 		        }
 		      }
-		      if (storageplaceUsed == false) {   
-		            
-		            String query = "DELETE FROM storageplace WHERE storageplace_key=?;";
-			         Connection conn = Database.connectMariaDb();
-			         try {
-						// Setup statement
-						 PreparedStatement stmt = conn.prepareStatement(query);
-		     
-						 // Set values
-						
-						stmt.setInt(1, storageplaceKey);
-						
-						// Execute statement
-						stmt.executeUpdate();
-						
-						// Closing statement and connection
-						stmt.close();
-						Database.mariaDbClose();
-					} catch (SQLException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-						return 0;
-					}
-
-		            return 1;   
-		         }
-		
+		      if (storageplaceUsed == false) {       
+	            String query = "DELETE FROM storageplace WHERE storageplace_key=?;";
+		         Connection conn = Database.connectMariaDb();
+		         try {
+					// Setup statement
+					 PreparedStatement stmt = conn.prepareStatement(query);     
+					 // Set values				
+					stmt.setInt(1, storageplaceKey);					
+					// Execute statement
+					stmt.executeUpdate();					
+					// Closing statement and connection
+					stmt.close();
+					Database.mariaDbClose();
+				} catch (SQLException e) {
+					System.err.println("An SQL exception occured when trying to delete from storageplace " + e.getMessage());
+					return 0;
+				}
+	            return 1;   
+	         }		
 		      return 0;
 		   }
-		
-		// Note that deleting a storageplace will remove also any linked items!
-				public int deleteStorageplaceOrg(int storageplaceKey){
-					
-				      List<Storageplace> storageplaceList = getAll();
 
-				      for(Storageplace storageplace: storageplaceList){
-				         if(storageplace.getStorageplaceKey() == storageplaceKey){
-				            int index = storageplaceList.indexOf(storageplace);			
-				            storageplaceList.remove(index);
-				            
-				            String query = "DELETE FROM storageplace WHERE storageplace_key=?;";
-					         Connection conn = Database.connectMariaDb();
-					         try {
-								// Setup statement
-								 PreparedStatement stmt = conn.prepareStatement(query);
-				     
-								 // Set values
-								
-								stmt.setInt(1, storageplaceKey);
-								
-								// Execute statement
-								stmt.executeUpdate();
-								
-								// Closing statement and connection
-								stmt.close();
-								Database.mariaDbClose();
-							} catch (SQLException e) {
-								// TODO Auto-generated catch block
-								e.printStackTrace();
-								return 0;
-							}
-
-				            return 1;   
-				         }
-				      }		
-				      return 0;
-				   }
-	
 }
